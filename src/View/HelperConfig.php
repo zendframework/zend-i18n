d07ae2654fa0abb723a1720bbd6044aa0022d748
maks feltrin <pine3ree@gmail.com>
2016-05-27 23:17:48 +0200

    use sm3 configure when possible
    to prevent any performance overhead.
    
    (same reasons for https://github.com/zendframework/zend-form/pull/77)

--- src/View/HelperConfig.php
+++ src/View/HelperConfig.php
@@ -74,12 +74,17 @@ class HelperConfig implements ConfigInterface
      */
     public function configureServiceManager(ServiceManager $serviceManager)
     {
-        foreach ($this->factories as $name => $factory) {
-            $serviceManager->setFactory($name, $factory);
+        if (method_exists($serviceManager, 'configure')) {
+            $serviceManager->configure($this->toArray());
+        } else {        
+            foreach ($this->factories as $name => $factory) {
+                $serviceManager->setFactory($name, $factory);
+            }
+            foreach ($this->aliases as $alias => $target) {
+                $serviceManager->setAlias($alias, $target);
+            }
         }
-        foreach ($this->aliases as $alias => $target) {
-            $serviceManager->setAlias($alias, $target);
-        }
+
         return $serviceManager;
     }
 
