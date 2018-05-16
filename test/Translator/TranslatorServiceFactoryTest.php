b0ba39da9f7458118854ae613b474bdd044f7bfb
Matthew Weier O'Phinney <matthew@zend.com>
2018-05-16 11:00:39 -0500

    Test that TranslatorPluginManager is only injected conditionally

--- test/Translator/TranslatorServiceFactoryTest.php
+++ test/Translator/TranslatorServiceFactoryTest.php
@@ -24,14 +24,35 @@ class TranslatorServiceFactoryTest extends TestCase
             ['config', []],
             ['TranslatorPluginManager', $pluginManagerMock]
         ];
+
         $serviceLocator = $this->createMock(ContainerInterface::class);
-        $serviceLocator->expects($this->exactly(2))
-                       ->method('get')
-                       ->will($this->returnValueMap($slContents));
+        $serviceLocator
+            ->expects($this->exactly(1))
+            ->method('has')
+            ->with($this->equalTo('TranslatorPluginManager'))
+            ->will($this->returnValue(true));
+        $serviceLocator
+            ->expects($this->exactly(2))
+            ->method('get')
+            ->will($this->returnValueMap($slContents));
 
         $factory = new TranslatorServiceFactory();
         $translator = $factory($serviceLocator, Translator::class);
         $this->assertInstanceOf(Translator::class, $translator);
         $this->assertSame($pluginManagerMock, $translator->getPluginManager());
+    }
+
+    public function testCreateServiceWithNoTranslatorPluginManagerDefined()
+    {
+        $serviceLocator = $this->createMock(ContainerInterface::class);
+        $serviceLocator
+            ->expects($this->exactly(1))
+            ->method('get')
+            ->with($this->equalTo('config'))
+            ->will($this->returnValue([]));
+
+        $factory = new TranslatorServiceFactory();
+        $translator = $factory($serviceLocator, Translator::class);
+        $this->assertInstanceOf(Translator::class, $translator);
     }
 }
