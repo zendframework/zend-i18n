1f1021bf9e1c82a60d7f6726bce5d71b5d6c18d3
Cvetomir Lazarov <cvetomir.lazarov@gmail.com>
2018-08-11 22:25:00 +0300

    Add helper trait to provide type-hinting for zend-view

---
+++ src/View/HelperTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zend\I18n\View;
+
+use IntlDateFormatter;
+
+// @codingStandardsIgnoreStart
+/**
+ * Trait HelperTrait
+ *
+ * The trait provides convenience methods for view helpers,
+ * defined by the zend-i18n component. It is designed to be used
+ * for type-hinting $this variable inside zend-view templates via doc blocks.
+ *
+ * The base class is PhpRenderer, followed by the helper trait from
+ * the zend-i18n component. However, multiple helper traits from different
+ * Zend components can be chained afterwards.
+ *
+ * @example @var \Zend\View\Renderer\PhpRenderer|\Zend\I18n\View\HelperTrait $this
+ *
+ * @method string currencyFormat($number, $currencyCode = null, $showDecimals = null, $locale = null, $pattern = null)
+ * @method string dateFormat($date, $dateType = IntlDateFormatter::NONE, $timeType = IntlDateFormatter::NONE, $locale = null, $pattern = null)
+ * @method string numberFormat($number, $formatStyle = null, $formatType = null, $locale = null, $decimals = null, array $textAttributes = null)
+ * @method string plural($strings, $number)
+ * @method string translate($message, $textDomain = null, $locale = null)
+ * @method string translatePlural($singular, $plural, $number, $textDomain = null, $locale = null)
+ */
+trait HelperTrait
+{
+}
