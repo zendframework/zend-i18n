7f3a1fc0134b8a92c8b72147f981885f7697c904
Cvetomir Lazarov <cvetomir.lazarov@gmail.com>
2018-09-05 12:04:57 +0300

    Add doc block with copyright and license information

--- src/View/HelperTrait.php
+++ src/View/HelperTrait.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-i18n for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-i18n/blob/master/LICENSE.md New BSD License
+ */
 
 namespace Zend\I18n\View;
 
