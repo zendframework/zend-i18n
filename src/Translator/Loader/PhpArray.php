801dd1117674303220ec5040e4cce82e07ca5f86
webimpress <contact@webimpress.com>
2019-09-29 22:34:25 +0100

    Fixes PHP 7.4 compatibility
    Using array_key_exists on object is deperecated. We can use `offsetExists`
    as it is the most adequate in these case - ArrayObject instances.

--- src/Translator/Loader/PhpArray.php
+++ src/Translator/Loader/PhpArray.php
@@ -47,7 +47,7 @@ class PhpArray extends AbstractFileLoader
 
         $textDomain = new TextDomain($messages);
 
-        if (property_exists('', $textDomain)) {
+        if ($textDomain->offsetExists('')) {
             if (isset($textDomain['']['plural_forms'])) {
                 $textDomain->setPluralRule(
                     PluralRule::fromString($textDomain['']['plural_forms'])
